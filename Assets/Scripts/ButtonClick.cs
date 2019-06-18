8944b0f481e8d7e463b5b76aac44b7d57f031cab
vsinghpratap98 <vsinghpratap98@gmail.com>
2019-06-18 10:32:10 +0530

    first commit

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public GameObject achievements;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void FBbtn()
+    {
+                Application.OpenURL("https://www.facebook.com/virendra.p.rathore");
+
+    }
+    public void Instabtn()
+    {
+        Application.OpenURL("https://www.instagram.com/vsinghpratap98/");
+
+    }
+    public void inbtn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/virendra-pratap-singh-rathore-77a838152/");
+    }
+    public void twibtn()
+    {
+        Application.OpenURL("https://twitter.com/vsinghpratap98");
+    }
+    public void achievement(){
+        achievements.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
