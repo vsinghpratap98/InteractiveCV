55689189a6659e543474c6af3503fcca12772f7a
vsinghpratap98 <vsinghpratap98@gmail.com>
2019-06-21 07:31:21 +0530

    image in panels and back buttons

---
+++ Assets/Scripts/OncollisionIEEE.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OncollisionIEEE : MonoBehaviour
+{
+    public GameObject image;
+    // Start is called before the first frame update
+    public void OnMouseDown(){
+        image.SetActive(true);
+    }
+    public void backbutton(){
+        image.SetActive(false);
+    }
+    
+}
