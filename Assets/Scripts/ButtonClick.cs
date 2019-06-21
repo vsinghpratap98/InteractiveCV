748d150bc57829ffde1888e89b31e672c592d3f6
vsinghpratap98 <vsinghpratap98@gmail.com>
2019-06-21 06:13:52 +0530

    added 3 buttons and 3 panels

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class ButtonClick : MonoBehaviour
 {
-    public GameObject achievements;
+    public GameObject ProfPanel;
+    public GameObject ExtraPanel;
+
+    public GameObject AcadePanel;
+
+    public GameObject Proachievements;
+    public GameObject Xtraachievements;
+    public GameObject Academicachievements;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +19,7 @@ public class ButtonClick : MonoBehaviour
     }
     public void FBbtn()
     {
-                Application.OpenURL("https://www.facebook.com/virendra.p.rathore");
+        Application.OpenURL("https://www.facebook.com/virendra.p.rathore");
 
     }
     public void Instabtn()
@@ -28,9 +35,38 @@ public class ButtonClick : MonoBehaviour
     {
         Application.OpenURL("https://twitter.com/vsinghpratap98");
     }
-    public void achievement(){
-        achievements.SetActive(true);
+    public void achievement()
+    {
+        Proachievements.SetActive(true);
+        Xtraachievements.SetActive(true);
+        Academicachievements.SetActive(true);
+
     }
+    public void OnClickPro()
+    {
+        ProfPanel.SetActive(true);
+                ExtraPanel.SetActive(false); 
+                        AcadePanel.SetActive(false);
+       
+
+    }
+    public void OnClickX()
+    {
+        ExtraPanel.SetActive(true);        
+        ProfPanel.SetActive(false);
+        AcadePanel.SetActive(false);
+
+    }
+    public void OnClickAcade()
+    {   ExtraPanel.SetActive(false);        
+        ProfPanel.SetActive(false);
+        AcadePanel.SetActive(true);
+    }
+    public void whtsappbtn()
+    {
+        Application.OpenURL("https://wa.me/<918076318675>");
+    }
+
 
     // Update is called once per frame
     void Update()
